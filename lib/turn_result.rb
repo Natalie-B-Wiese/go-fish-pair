d5a1bb0caec73a2ffc3a99a7518f68f798c6ec4a
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-06-13 11:28:19 -0400

    Turn result with book

---
+++ lib/turn_result.rb
@@ -0,0 +1,59 @@
+class TurnResult
+  attr_reader :current_player, :opponent_player, :rank_requested, :cards_received_opponent, :card_received_deck,
+              :was_book_made
+
+  def initialize(current_player:, opponent_player: nil, rank_requested: nil,
+                 cards_received_opponent: [], card_received_deck: nil, was_book_made: false)
+    @current_player = current_player
+    @opponent_player = opponent_player
+    @rank_requested = rank_requested
+    @cards_received_opponent = cards_received_opponent
+    @card_received_deck = card_received_deck
+    @was_book_made = was_book_made
+  end
+
+  def message(player)
+    if book_made?
+      book_message(player)
+    else
+      puts 'other'
+    end
+  end
+
+  private
+
+  # TODO: handle if deck is empty
+  def rank_received
+    if went_fish?
+      card_received_deck.rank
+    else
+      cards_received_opponent.first.rank
+    end
+  end
+
+  def book_message(player)
+    "#{player_to_s(player)} made a book with four #{rank_received}s!"
+  end
+
+  def book_made?
+    !!was_book_made
+  end
+
+  def went_fish?
+    cards_received_opponent.empty?
+  end
+
+  def opponent_to_s(you_player, is_subject = true)
+    player_variable_to_s(opponent_player, you_player, is_subject)
+  end
+
+  def player_to_s(you_player, is_subject = true)
+    player_variable_to_s(current_player, you_player, is_subject)
+  end
+
+  def player_variable_to_s(variable_player, you_player, is_subject)
+    you = 'You'
+    you = you.downcase unless is_subject
+    variable_player == you_player ? you : variable_player.name
+  end
+end
---
+++ spec/turn_result_spec.rb
@@ -0,0 +1,37 @@
+require_relative '../lib/turn_result'
+require_relative '../lib/player'
+require_relative '../lib/card'
+
+# initialize(current_player:, opponent_player: nil, rank_requested: nil,
+#                  cards_received_opponent: [], card_received_deck: nil, was_book_made: false)
+describe TurnResult do
+  describe '#message' do
+    let(:current_player_name) { 'Jeff' }
+    let(:current_player) { Player.new(current_player_name) }
+
+    let(:opponent_player_name) { 'Bob' }
+    let(:opponent_player) { Player.new(opponent_player_name) }
+
+    let(:other_player_name) { 'Henry' }
+    let(:other_player) { Player.new(other_player_name) }
+
+    let(:card_received) { Card.new('5', 'Spades') }
+
+    context 'when book_made? true and went_fish? true' do
+      let(:turn_result) do
+        TurnResult.new(current_player: current_player, opponent_player: opponent_player,
+                       card_received_deck: card_received, was_book_made: true)
+      end
+
+      it 'contains a book message' do
+        result = turn_result.message(other_player)
+        expect(result).to match(/book/i)
+      end
+
+      it 'shows book rank' do
+        result = turn_result.message(other_player)
+        expect(result).to match(/#{card_received.rank}/)
+      end
+    end
+  end
+end
